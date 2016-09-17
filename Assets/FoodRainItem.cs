d087ba3f6dd164b25477ccd48f12fc2349525d95
Rebecca Ansems <ansems_rebecca@hotmail.com>
2016-09-17 14:56:35 -0300

    Added more raining food

---
+++ Assets/FoodRainItem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodRainItem : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+	    if(transform.position.y < -1)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
