d668f273c7cfc7330a80e1caca53fec0e615bb9c
Rebecca Ansems <ansems_rebecca@hotmail.com>
2016-09-17 14:56:28 -0300

    Added raining food

--- Assets/CatCommands.cs
+++ Assets/CatCommands.cs
@@ -4,16 +4,20 @@ using System.Collections;
 public class CatCommands : MonoBehaviour {
 
     public GameObject cat;
-    bool runAway = false;
+    bool runAway = false, catTurned = false;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (OVRInput.Get(OVRInput.Button.One) || Input.GetKeyDown(KeyCode.Space))
+        if (OVRInput.Get(OVRInput.Button.DpadUp) || Input.GetKey(KeyCode.W))
+        {
+            CommandHeard("Jump");
+        }
+
+        if (OVRInput.Get(OVRInput.Button.DpadLeft) || Input.GetKey(KeyCode.D))
         {
             CommandHeard("Fetch");
         }
@@ -26,6 +30,22 @@ public class CatCommands : MonoBehaviour {
         if (runAway)
         {
             cat.transform.Translate(Vector3.forward * Time.deltaTime * 10);
+
+            if (cat.transform.position.z > 100)
+            {
+                Vector3 rot = cat.transform.rotation.eulerAngles;
+                rot = new Vector3(rot.x, rot.y + 180, rot.z);
+                cat.transform.rotation = Quaternion.Euler(rot);
+                catTurned = true;
+                FoodExplosion();
+            }
+
+            if (cat.transform.position.z < 0 && catTurned)
+            {
+                runAway = false;
+                cat.GetComponent<Animator>().SetInteger("CatCommands", 0);
+                catTurned = false;
+            }
         }
 
         //reset animation
@@ -44,13 +64,25 @@ public class CatCommands : MonoBehaviour {
         else if (command.Equals("Meow"))
         {
         }
-        else if (command.Equals("Fetch"))
+        else if (command.Equals("Fetch") && !runAway)
         {
             cat.GetComponent<Animator>().SetInteger("CatCommands", 2);
             runAway = true;
+            catTurned = false;
             Vector3 rot = cat.transform.rotation.eulerAngles;
             rot = new Vector3(rot.x, rot.y + 180, rot.z);
             cat.transform.rotation = Quaternion.Euler(rot);
+        }
+    }
+
+    public GameObject[] food;
+
+    void FoodExplosion()
+    {
+        int foodItem = Random.Range(0, 4);
+        for(int i=0; i< Random.Range(350, 500); i++)
+        {
+            Instantiate(food[foodItem], new Vector3(Random.Range(-15, 15), Random.Range(500, 650), Random.Range(-35, 15)), Quaternion.identity);
         }
     }
 }
