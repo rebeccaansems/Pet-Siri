06b494348809e2626b45b98cb5724edb191622f3
Rebecca Ansems <ansems_rebecca@hotmail.com>
2016-09-18 00:48:29 -0300

    Added ability to control cat with audio
    IBM's Watson (speech to text)

--- Assets/CatCommands.cs
+++ Assets/CatCommands.cs
@@ -1,17 +1,25 @@
-﻿ using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
+using System;
+using UnitySpeechToText.Services;
 
 public class CatCommands : MonoBehaviour {
 
     public GameObject cat;
     bool runAway = false, catTurned = false;
+    public WatsonStreamingSpeechToTextService m_SpeechToTextService;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+        m_SpeechToTextService.RegisterOnError(OnError);
+        m_SpeechToTextService.RegisterOnTextResult(OnTextResult);
+        m_SpeechToTextService.RegisterOnRecordingTimeout(OnRecordingTimeout);
+        m_SpeechToTextService.StartRecording();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         if (OVRInput.Get(OVRInput.Button.DpadUp) || Input.GetKey(KeyCode.W))
         {
             CommandHeard("Jump");
@@ -90,14 +98,14 @@ public class CatCommands : MonoBehaviour {
 
     void CommandHeard(string command)
     {
-        if (command.Equals("Jump"))
+        if (command.Contains("jump")|| command.Contains("john"))//jump
         {
             cat.GetComponent<Animator>().SetInteger("CatCommands", 1);
         }
-        else if (command.Equals("Meow"))
+        else if (command.Contains("Meow"))
         {
         }
-        else if (command.Equals("Fetch") && !runAway)
+        else if ((command.Contains("fetch") || command.Contains("that")) && !runAway)//fetch
         {
             cat.GetComponent<Animator>().SetInteger("CatCommands", 2);
             runAway = true;
@@ -109,13 +117,34 @@ public class CatCommands : MonoBehaviour {
 
     void FoodExplosion()
     {
-        int foodItem = Random.Range(0, 4);
+        int foodItem = UnityEngine.Random.Range(0, 4);
         if (!catTurned)
         {
-            for (int i = 0; i < Random.Range(750, 1000); i++)
+            for (int i = 0; i < UnityEngine.Random.Range(750, 1000); i++)
             {
-                Instantiate(food[foodItem], new Vector3(Random.Range(-50, 50), Random.Range(750, 1000), Random.Range(-65, 50)), Quaternion.identity);
+                Instantiate(food[foodItem], new Vector3(UnityEngine.Random.Range(-50, 50), UnityEngine.Random.Range(750, 1000), UnityEngine.Random.Range(-65, 50)), Quaternion.identity);
             }
         }
+    }
+
+    void OnError(string text)
+    {
+        Debug.LogError(text);
+    }
+
+    // Note that handling interim results is only necessary if your speech-to-text service is streaming.
+    // Non-streaming speech-to-text services should only return one result per recording.
+    void OnTextResult(SpeechToTextResult result)
+    {
+        for (int i = 0; i < result.TextAlternatives.Length; ++i)
+        {
+            Debug.Log(result.TextAlternatives[i].Text);
+            CommandHeard(result.TextAlternatives[i].Text);
+        }
+    }
+
+    void OnRecordingTimeout()
+    {
+        Debug.Log("Timeout");
     }
 }
